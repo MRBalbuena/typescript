5dc291d7204153b5dde1041b53c3176d4a14e0cc
mrbalbuena <mrbalbuena@hotmail.com>
2015-12-09 17:29:52 +0000

    added auto ts sample - inconcluse

---
+++ TS2013/Scripts/auto.ts
@@ -0,0 +1,23 @@
+﻿class AutoEngine {
+    constructor(public horsePower: number, public engineType: string) { }
+
+    start(callback: (startStatus: boolean, engineType: string) => void) {
+        window.setTimeout(() => {
+            callback(true, this.engineType);
+        }, 1000);
+    }
+
+    stop(callback: (stopStatus: boolean, engineType: string) => void) {
+        window.setTimeout(() => {
+            callback(true, this.engineType);
+        }, 1000);
+    }
+}
+
+class Accessory {
+    constructor(public accessoryNumber: number, public title: string) { }
+}
+
+class Auto {
+    private _basePrice: number;
+}
